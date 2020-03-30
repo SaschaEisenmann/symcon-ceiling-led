92bd290300a8e01157c75995d5c08a959e4f2b92
Sascha Eisenmann <sascha@eisenmann.it>
2020-03-30 11:56:42 +0200

    Created basic module

--- LedController/module.php
+++ LedController/module.php
@@ -32,7 +32,7 @@ class LedController extends IPSModule implements iAdapter
     {
         $this->RequireParent("{6DC3D946-0D31-450F-A8C6-C42DB8D7D4F1}");
 
-        $this->RegisterTimer('Schedule', 0, 'LEDC_Interval($_IPS[\'TARGET\'], 0);');
+        $this->RegisterTimer('Schedule', 0, 'LEDC_Interval($_IPS[\'TARGET\']);');
     }
 
     public function ApplyChanges()
