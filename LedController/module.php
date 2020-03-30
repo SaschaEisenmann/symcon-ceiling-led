f51ed7a5cfe31353155f07c2fd3c90d56cd88362
Sascha Eisenmann <sascha@eisenmann.it>
2020-03-30 08:15:07 +0200

    Created basic module

---
+++ LedController/module.php
@@ -0,0 +1,23 @@
+<?php
+class LedController extends IPSModule
+{
+    public function Create()
+    {
+        parent::Create();
+
+        $this->RequireParent("{6179ED6A-FC31-413C-BB8E-1204150CF376}");
+    }
+
+    public function ApplyChanges()
+    {
+        parent::ApplyChanges();
+    }
+
+    public function Enable()
+    {
+    }
+
+    public function Disable()
+    {
+    }
+}
