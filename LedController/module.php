3862d16f54dbb819c41793f28f76ac919a5c2d24
Sascha Eisenmann <sascha@eisenmann.it>
2020-03-30 13:33:10 +0200

    Created basic module

--- LedController/module.php
+++ LedController/module.php
@@ -12,10 +12,7 @@ class LedController extends IPSModule implements iAdapter
         parent::Create();
 
         $this->RequireParent("{6DC3D946-0D31-450F-A8C6-C42DB8D7D4F1}");
-
-        $this->RegisterPropertyString('Mode', 'OFF');
-        $this->RegisterPropertyString('PARAMETERS', json_encode([]));
-        $this->RegisterTimer('Schedule', 0, 'LEDC_Interval($_IPS[\'TARGET\']);');
+        $this->RegisterTimer('SCHEDULE', 0, 'LEDC_TriggerInterval($_IPS[\'TARGET\']);');
     }
 
     public function ApplyChanges()
@@ -24,6 +21,7 @@ class LedController extends IPSModule implements iAdapter
 
         $this->RegisterVariableInteger('MODE', 'Mode', "", 0);
         $this->RegisterVariableString('PARAMETERS', 'Parameters', "", 1);
+        $this->RegisterVariableString('STATE', 'Parameters', "", 3);
         // 0 -> Off
         // 1 -> Color
         // 2 -> ColorChange
@@ -38,36 +36,122 @@ class LedController extends IPSModule implements iAdapter
         SetValueInteger($this->GetIDForIdent("MODE"), $mode);
         SetValueString($this->GetIDForIdent("PARAMETERS"), json_encode($parameters));
 
-        $this->switchMode();
+        $this->TriggerMode($mode, false);
     }
 
-    private function switchMode() {
-        $mode = GetValueInteger($this->GetIDForIdent("MODE"));
-        $parameters = json_decode(GetValueString($this->GetIDForIdent("PARAMETERS")));
-
+    private function TriggerMode($mode, $isInterval)
+    {
         switch ($mode) {
             case 1:
-                $this->switchToModeColor($parameters);
+                $this->modeColor($isInterval);
+                return;
+            case 2:
+                $this->modeColorChange($isInterval);
                 return;
             default:
-                $this->switchToModeOff($parameters);
+                $this->modeOff($isInterval);
                 return;
         }
     }
 
-    private function switchToModeOff($parameters) {
-        $this->SetBatch(0, 0, 0);
-    }
-
-    private function switchToModeColor($parameters) {
+    private function modeColor($isInterval) {
+        $parameters = $this->LoadParameters();
         $this->SetBatch($parameters[0], $parameters[1], $parameters[2]);
     }
 
+    private function modeOff($isInterval) {
+        $this->SetBatch(0, 0, 0);
+    }
+
+    private function modeColorChange($isInterval) {
+
+        if(!$isInterval) {
+            $this->SetTimerInterval('SCHEDULE', 100);
+        } else {
+            $hue = $this->LoadState()['hue'];
+
+            $color = $this->GetRgb($hue, 100, 100);
+            $this->SetBatch($color['red'], $color['green'], $color['blue']);
+
+            $hue += 2;
+            if($hue > 359) {
+                $hue = 1;
+            }
+
+            $this->SaveState(array(
+               'hue' => $hue
+            ));
+        }
+    }
+
+    public function TriggerInterval()
+    {
+        $mode = GetValueInteger($this->GetIDForIdent("MODE"));
+        $this->TriggerMode($mode, true);
+    }
+
+    private function LoadParameters() {
+        return json_decode(GetValueString($this->GetIDForIdent("PARAMETERS")));
+    }
+
+    private function LoadState() {
+        return json_decode(GetValueString($this->GetIDForIdent("STATE")));
+    }
+
+    private function SaveState($state) {
+        SetValueString($this->GetIDForIdent("STATE"), json_encode($state));
+    }
 
 
 
 
 
+
+    private function GetRgb($iH, $iS, $iV) {
+
+        if($iH < 0)   $iH = 0;   // Hue:
+        if($iH > 360) $iH = 360; //   0-360
+        if($iS < 0)   $iS = 0;   // Saturation:
+        if($iS > 100) $iS = 100; //   0-100
+        if($iV < 0)   $iV = 0;   // Lightness:
+        if($iV > 100) $iV = 100; //   0-100
+
+        $dS = $iS/100.0; // Saturation: 0.0-1.0
+        $dV = $iV/100.0; // Lightness:  0.0-1.0
+        $dC = $dV*$dS;   // Chroma:     0.0-1.0
+        $dH = $iH/60.0;  // H-Prime:    0.0-6.0
+        $dT = $dH;       // Temp variable
+
+        while($dT >= 2.0) $dT -= 2.0; // php modulus does not work with float
+        $dX = $dC*(1-abs($dT-1));     // as used in the Wikipedia link
+
+        switch(floor($dH)) {
+            case 0:
+                $dR = $dC; $dG = $dX; $dB = 0.0; break;
+            case 1:
+                $dR = $dX; $dG = $dC; $dB = 0.0; break;
+            case 2:
+                $dR = 0.0; $dG = $dC; $dB = $dX; break;
+            case 3:
+                $dR = 0.0; $dG = $dX; $dB = $dC; break;
+            case 4:
+                $dR = $dX; $dG = 0.0; $dB = $dC; break;
+            case 5:
+                $dR = $dC; $dG = 0.0; $dB = $dX; break;
+            default:
+                $dR = 0.0; $dG = 0.0; $dB = 0.0; break;
+        }
+
+        $dM  = $dV - $dC;
+        $dR += $dM; $dG += $dM; $dB += $dM;
+        $dR *= 255; $dG *= 255; $dB *= 255;
+
+        return array(
+            'red' => $dR,
+            'green' => $dG,
+            'blue' => $dB
+        );
+    }
 
     private function FindMode($name) {
         foreach ($this->modes as $mode) {
